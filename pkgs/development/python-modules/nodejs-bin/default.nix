fc760daf11b85f9dfae0d7f45446c1d2b5ab2da8
Vinny Meller <vinnymeller@proton.me>
2024-04-08 23:27:03 -0400

    add autopatchelfhook to nodejs-bin?

--- pkgs/development/python-modules/nodejs-bin/default.nix
+++ pkgs/development/python-modules/nodejs-bin/default.nix
@@ -1,4 +1,5 @@
 {
+  autoPatchelfHook,
   lib,
   fetchurl,
   buildPythonPackage,
@@ -12,6 +13,8 @@ buildPythonPackage {
     url = "https://files.pythonhosted.org/packages/14/f5/b85f10ddb2b6bf58395bd08a7794ded91518f7eca1dc771c22c808c44e81/nodejs_bin-18.4.0a4-py3-none-manylinux_2_12_x86_64.manylinux2010_x86_64.whl";
     sha256 = "06cfeaa4d26eec94d8edb9927525ce94eb96dadc81f7d1daed42d1a7d003a4c9";
   };
+
+  nativeBuildInputs = [autoPatchelfHook];
 
   pythonImportsCheck = ["nodejs"];
 
